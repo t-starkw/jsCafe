3b78281ffcc7481e0112044e8da88166c79547c4
unknown <carter.adrian@gmail.com>
2023-03-01 18:46:13 -0500

    add models

---
+++ server/models/Merch.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('mongoose');
+
+const merchSchema = new Schema(
+
+    {
+        id:{
+            type: number,
+            required: false,
+            unique: true
+        },
+        product_name: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Decimal128,
+            required: true,
+            validate: {
+                isDecimal: true
+            }
+        },
+        stock: {
+            type: number,
+            required: true,
+            defaultValue: 10,
+            validate: {
+                isNumeric: true
+            }
+        },
+        category_id: {
+            type: String,
+            ref: { 'category'
+            }
+          }
+        },
+);
+
+
+module.exports = Merch;
