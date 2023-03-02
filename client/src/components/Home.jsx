3f6d44acd19e7f3dfc1b20baeb45397eb4837a19
Sean M Fitzpatrick <s3an.m4l.f1tz@gmail.com>
2023-03-02 17:28:04 -0500

    the cube is back

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import About from "./About";
+import Menu from "./Menu";
+import Navbar from "./Navbar";
+import { Canvas } from "@react-three/fiber";
+import { Experience } from "./Experience";
+
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
---
+++ client/src/components/Menu.js
@@ -0,0 +1,4 @@
+import About from './About';
+import Home from './Home';
+import Menu from './Menu';
+import Navbar from './Navbar';
