300ad39235ede36b21e548e229445e207a2409ba
Zack <zachary.berkley@gmail.com>
2023-03-04 21:03:39 -0500

    add queries

---
+++ client/src/utils/queries.js
@@ -0,0 +1,32 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_PROFILES = gql`
+  query allProfiles {
+    profiles {
+      _id
+      name
+      email
+    }
+  }
+`;
+
+export const QUERY_SINGLE_PROFILE = gql`
+  query singleProfile($profileId: ID!) {
+    profile(profileId: $profileId) {
+      _id
+      name
+      email
+      order_history
+    }
+  }
+`;
+
+export const QUERY_MENU_ITEMS = gql`
+    query allMenuItems {
+        menu {
+            _id
+            product_name
+            price
+        }
+    }    
+`
