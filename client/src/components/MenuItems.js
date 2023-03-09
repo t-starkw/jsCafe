22a6d225c1a4f9201d748f27658bed25b1e39b37
Sean M Fitzpatrick <s3an.m4l.f1tz@gmail.com>
2023-03-09 16:30:03 -0500

    updates for all pages - pics

--- client/src/components/MenuItems.js
+++ client/src/components/MenuItems.js
@@ -1,112 +1,105 @@
-import React from 'react';
+import React from "react";
 
 import { useState } from "react";
 import { useMutation } from "@apollo/client";
-import { ADD_HISTORY } from "../utils/mutations"
+import { ADD_HISTORY } from "../utils/mutations";
 import Auth from "../utils/auth";
 import espresso from "../images/espresso2.jpeg";
 
 function MenuItems(props) {
+  // shopping cart, does cart need to be an [] or a {}?
+  const [cart, setCart] = useState([]);
+  const [cartDiv, setDiv] = useState({});
 
-    // shopping cart, does cart need to be an [] or a {}?
-    const [cart, setCart] = useState([]);
-    const [cartDiv, setDiv] = useState({});
+  function addToCart(x) {
+    const updateCart = [
+      ...cart,
+      {
+        _id: x.id,
+        product_name: x.product_name,
+        price: x.price,
+      },
+    ];
+    setCart(updateCart);
+  }
 
-    function addToCart(x) {
-        const updateCart = [
-            ...cart,
-            {
-                _id: x.id,
-                product_name: x.product_name,
-                price: x.price,
-            }
-        ]
-        setCart(updateCart)
+  const [addToHistory] = useMutation(ADD_HISTORY);
+
+  const handleCheckout = async (cart) => {
+    const order = cart;
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    console.log(token);
+    if (!token) {
+      return false;
     }
-
-    const [addToHistory] = useMutation(ADD_HISTORY)
-
-    const handleCheckout = async (cart) => {
-        const order = cart
-        const token = Auth.loggedIn() ? Auth.getToken() : null
-        console.log(token)
-        if (!token) {
-            return false;
-        }
-        try {
-            await addToHistory({
-                variables: {input: order},
-            })
-        } catch(err) {
-            console.log(JSON.stringify(err, null, 2));
-        }
+    try {
+      await addToHistory({
+        variables: { input: order },
+      });
+    } catch (err) {
+      console.log(JSON.stringify(err, null, 2));
     }
+  };
 
-    return (
-        <>
-            {props.items.map((x) => (
+  return (
+    <>
+      {props.items.map((x) => (
+        <div className="m-10 w-full max-w-sm bg-gray-300 border border-gray-200 rounded-lg shadow">
+          <a href="#">
+            <img className="p-8 rounded-t-lg" src={espresso} alt="" />
+          </a>
+          <div className="px-5 pb-5">
+            <a href="#">
+              <h5 className="text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                {x.product_name}
+              </h5>
+            </a>
+            <div className="flex items-center mt-2.5 mb-5"></div>
+            <div className="flex items-center justify-between">
+              <span className="text-3xl font-bold text-gray-900 dark:text-white">
+                ${x.price}
+              </span>
+              <a
+                href="#"
+                className="text-white bg-teal-600 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                onClick={() => {
+                  console.log(x);
 
-                <div className="m-10 w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img
-                            className="p-8 rounded-t-lg"
-                            src={espresso}
-                            alt=""
-                        />
-                    </a>
-                    <div className="px-5 pb-5">
-                        <a href="#">
-                            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                                {x.product_name}
-                            </h5>
-                        </a>
-                        <div className="flex items-center mt-2.5 mb-5">
-                        </div>
-                        <div className="flex items-center justify-between">
-                            <span className="text-3xl font-bold text-gray-900 dark:text-white">
-                                ${x.price}
-                            </span>
-                            <a
-                                href="#"
-                                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                                onClick={() => {
-
-                                    console.log(x)
-
-                                    addToCart(x);
-                                    console.log(cart);
-
-                                }}
-                            >
-                                Add to cart
-                            </a>
-                        </div>
-                    </div>
-
-                </div>
-            ))}
-            <div>
-                <div className="m-10 w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    Your shopping cart:
-                    {cart.map((cartItem) => (
-                        <ul>
-                            <li>{cartItem.product_name}  ${cartItem.price}</li>
-                        </ul>
-                    ))}
-                </div>
-                <div>
-                    <a
-                        href="#"
-                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                        onClick={() => {
-                            console.log(cart)
-                            handleCheckout(cart)
-                        }}>
-                        Checkout
-                    </a>
-                </div>
+                  addToCart(x);
+                  console.log(cart);
+                }}
+              >
+                Add to cart
+              </a>
             </div>
-        </>
-    )
+          </div>
+        </div>
+      ))}
+      <div>
+        <div className="m-10 w-full pl-2 max-w-sm static font-bold text-xl bg-gray-300 border border-gray-200 rounded-lg shadow">
+          Your shopping cart:
+          {cart.map((cartItem) => (
+            <ul>
+              <li>
+                {cartItem.product_name} ${cartItem.price}
+              </li>
+            </ul>
+          ))}
+          <div className="flex items-center pt-20 pb-5 justify-center">
+            <a
+              href="#"
+              className="text-white static bg-teal-600 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              onClick={() => {
+                console.log(cart);
+                handleCheckout(cart);
+              }}
+            >
+              Checkout
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 export default MenuItems;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../utils/mutations';
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
 import { Link } from "react-router-dom";
-
 import Auth from "../utils/auth";
-import Navbar from "../Navbar";
 
 const Login = (props) => {
+  const [darkMode, setDarkMode] = useState(false);
   const [formState, setFormState] = useState({ email: "", password: "" });
   const [login, { error, data }] = useMutation(LOGIN_USER);
 
@@ -42,54 +41,53 @@ const Login = (props) => {
   };
   return (
     <>
-      <div className="relative w-screen h-screen bg-pink-400">
-        <div className="flex justify-center items-center h-full flex-wrap content-around">
-          <div>
-            <form
-              className="max-w-[400px] w-full mx-auto bg-white rounded px-10 py-2"
-              onSubmit={handleFormSubmit}
-            >
-              <h2 className="text-4xl font-bold text-center py-4">Log In</h2>
-
-              <div className="flex flex-col mb-1">
-                <label>Email</label>
-                <input
-                  className="border relative bg-blue-200 p-2 form-input"
-                  placeholder="Your email"
-                  name="email"
-                  type="email"
-                  value={formState.email}
-                  onChange={handleChange}
-                />
-              </div>
-
-              <div className="flex flex-col mb-1">
-                <label>Password</label>
-                <input
-                  className="border relative bg-blue-400 p-2 form-input"
-                  type="password"
-                  placeholder="******"
-                  name="password"
-                  value={formState.password}
-                  onChange={handleChange}
-                />
-              </div>
-
-              <button
-                className="w-full py-3 mt-8 bg-blue-600 relative text-white "
-                style={{ cursor: "pointer" }}
-                type="submit"
+      <div className={darkMode ? "dark" : ""}>
+        <div className="w-screen h-screen bg-gray-600 text-gray-800 dark:bg-gray-400">
+          <div className="flex justify-center items-center h-full flex-wrap content-around">
+            <div>
+              <form
+                className="w-96 h-96 mx-auto bg-gray-400 rounded-3xl px-14 py-2"
+                onSubmit={handleFormSubmit}
               >
-                Sign In
-              </button>
-              {/* <p className="flex items-center mt-2">
-                            <input className="mr-2" type="checkbox" />
-                            Remember Me
-                        </p> */}
+                <h2 className="text-4xl font-bold text-center text-gray-800 py-4">Log In</h2>
 
-              {/* <p className="text-center mt-8">Not a Member? Sign up now</p> */}
-              <Link to="/signup">Signup</Link>
-            </form>
+                <div className="pt-8 flex flex-col text-gray-800 mb-1">
+                  <input
+                    className="border relative bg-teal-600 hover:bg-teal-800 text-gray-800 p-3 rounded-3xl form-input"
+                    placeholder="Your Email"
+                    name="email"
+                    type="email"
+                    value={formState.email}
+                    onChange={handleChange}
+                  />
+                </div>
+
+                <div className="flex flex-col mb-1 text-gray-800">
+                  <input
+                    className="border relative bg-teal-600 hover:bg-teal-800 text-gray-800 p-3 rounded-3xl form-input"
+                    type="password"
+                    placeholder="Your Password"
+                    name="password"
+                    value={formState.password}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="flex items-center justify-center pt-5"> 
+                <button
+                  className="w-2/5 py-3  bg-teal-400 hover:bg-teal-800 relative rounded-3xl text-gray-100 "                  style={{ cursor: "pointer" }}
+                  type="submit"
+                >
+                  Sign In
+                </button>
+                <button
+                  className="w-2/5  py-3  bg-teal-400 hover:bg-teal-800 relative rounded-3xl text-gray-100 "                  style={{ cursor: "pointer" }}
+                  type="submit"
+                >
+                  <Link to="/signup">Sign up</Link>
+                </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
