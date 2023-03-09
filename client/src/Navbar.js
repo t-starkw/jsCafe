e00a205bf9d649e261021487e811e8e11991244b
Tessa <tess.starkweather@gmail.com>
2023-03-08 20:30:42 -0500

    tooltip & checkout attempt

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import 'react-tooltip/dist/react-tooltip.css'
 import { Link, useMatch, useResolvedPath, useLocation } from "react-router-dom";
 import { BiFoodMenu, BiCoffee } from "react-icons/bi";
 import { CgProfile } from "react-icons/cg";
 import { TbPigMoney } from "react-icons/tb";
 import { MdDarkMode, MdLogout, MdLogin } from "react-icons/md";
+import { Tooltip } from "react-tooltip";
 import "./styles/App.css";
 import Auth from "./utils/auth";
 
@@ -12,32 +14,42 @@ export default function Navbar({ props }) {
     <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
       <nav>
         <ul>
-          <CustomLink to="/">
+          <CustomLink data-tooltip-id="home" data-tooltip-content="Home" to="/">
             <SideBarIcon icon={<BiCoffee size="28" />} />
           </CustomLink>
-          <CustomLink to="menu">
+          <Tooltip id="home" place="right" />
+          <CustomLink to="menu" data-tooltip-id="menu" data-tooltip-content="Menu">
             <SideBarIcon icon={<BiFoodMenu size="28" name="menu" />} />
           </CustomLink>
-          <CustomLink to="order">
+          <Tooltip id="menu" place="right" />
+          <CustomLink to="order" data-tooltip-id="order" data-tooltip-content="Order">
             <SideBarIcon icon={<TbPigMoney size="28" />} />
           </CustomLink>
-          <SideBarIcon icon={<MdDarkMode size="28" />} />
+          <Tooltip id="order" place="right" />
+          <SideBarIcon data-tooltip-id="dark" data-tooltip-content="DarkMode" icon={<MdDarkMode size="28" />} />
+          <Tooltip id="dark" place="right" />
           {/* To select last children */}
           <div className="fixed bottom-0 left-0 w-16 m-0 flex flex-col">
             {Auth.loggedIn() ? (
               <>
-                <CustomLink to="profile">
+
+                {/* This is where I added profile @Tessa */}
+                <CustomLink to="profile" data-tooltip-id="profile" data-tooltip-content="Profile">
                   <SideBarIcon icon={<CgProfile size="28" />} />
                 </CustomLink>
+                <Tooltip id="profile" place="right"/>
                 {/* Logout */}
-                <CustomLink onClick={Auth.logout}>
+                <CustomLink onClick={Auth.logout} data-tooltip-id="logout" data-tooltip-content="Logout">
                   <SideBarIcon icon={<MdLogout size="28" />} />
                 </CustomLink>
+                <Tooltip id="logout" place="right"/>
               </>
             ) : (
-              <CustomLink to="login">
+              <CustomLink to="login" data-tooltip-id="login" data-tooltip-content="Login/Signup">
                 <SideBarIcon icon={<MdLogin size="28" />} />
+                <Tooltip id="login" place="right" />
               </CustomLink>
+              
             )}
           </div>
         </ul>
