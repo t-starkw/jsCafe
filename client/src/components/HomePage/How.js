329c814253487a5c9b470abdcfe40df0cc17c6af
Sean M Fitzpatrick <s3an.m4l.f1tz@gmail.com>
2023-03-08 20:13:38 -0500

    the god push

--- client/src/components/HomePage/How.js
+++ client/src/components/HomePage/How.js
@@ -1,16 +1,136 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import styled from "styled-components";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Sphere, MeshDistortMaterial } from "@react-three/drei";
+import pot from "../../public/home-pot.png"
+
+const Section = styled.div`
+  font-family: monospace;
+  height: 100vh;
+  scroll-snap-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #ebedef;
+  justify-content: space-between;
+  @media only screen and (max-width: 768px) {
+    height: 200vh;
+  }
+`;
+
+const Container = styled.div`
+  height: 100%;
+  scroll-snap-align: center;
+  width: 100vw;
+  display: flex;
+  justify-content: space-between;
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+const Right = styled.div`
+  flex: 3;
+  float: left;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+  @media only screen and (max-width: 768px) {
+    flex: 1;
+    align-items: center;
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 74px;
+  color: #36393f;
+  @media only screen and (max-width: 768px) {
+    text-align: center;
+  }
+`;
+
+const Subtitle = styled.h2`
+  font-size: 36px;
+  color: #2DD4BF;
+`;
+
+const Desc = styled.p`
+  font-size: 24px;
+  color: #36393f;
+  @media only screen and (max-width: 768px) {
+    padding: 20px;
+    text-align: center;
+  }
+`;
+
+
+const Left = styled.div`
+  flex: 3;
+  position: relative;
+  @media only screen and (max-width: 768px) {
+    flex: 1;
+    width: 100%;
+  }
+`;
+
+const Img = styled.img`
+  width: 200px;
+  height: 200px;
+  object-fit: contain;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  animation: animate 2s infinite ease alternate;
+  @media only screen and (max-width: 768px) {
+    width: 100px;
+    height: 100px;
+  }
+  @keyframes animate {
+    to {
+      transform: translateY(20px);
+    }
+  }
+`;
 
 const How = () => {
-    return (
-        <>
-        <div 
-        className="
-        h-screen text-center
-        snap-center
-        "
-        >How</div>
-        </>
-    )
-}
+  return (
+    <Section>
+      <Container>
+        <Left>
+        <Canvas>
+            <Suspense fallback={null}>
+              <OrbitControls enableZoom={false} />
+              <ambientLight intensity={1.5} />
+              <directionalLight position={[2]} />
+              <Sphere args={[1, 100, 200]} scale={2}>
+                <MeshDistortMaterial
+                  color="#2DD4BF"
+                  attach="material"
+                  distort={0.5}
+                  speed={2}
+                />
+              </Sphere>
+            </Suspense>
+          </Canvas>
+          <Img src={pot} />
+        </Left>
+        <Right>
+        <Title>JsCafe</Title>
+            <Subtitle>Heres how it works!-</Subtitle>
+          <Desc>
+            Kelly this section is for you
+          </Desc>
+        </Right>
+      </Container>
+    </Section>
+  );
+};
 
 export default How;
